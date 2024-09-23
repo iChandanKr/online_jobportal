71bc81221f25c59398dbaa6ee14ab9f994c62b79
Chandan Kumar <chandankumar995584@gmail.com>
2024-09-23 13:32:55 +0530

    userModel created

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const userRoute = require("./users/user.route");
+const userRoute = require("./users/users.route");
 
 app.use(express.json());
 app.use("/api/v1/users", userRoute);
