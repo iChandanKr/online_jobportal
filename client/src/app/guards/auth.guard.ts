1e3e290be24c3b926b8f95e2164b40d0ea74d200
Chandan Kumar <chandankumar995584@gmail.com>
2024-10-09 15:34:25 +0530

    auth guard implemented using backend api

--- client/src/app/guards/auth.guard.ts
+++ client/src/app/guards/auth.guard.ts
@@ -1,15 +1,22 @@
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
+import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
-  const isAuthenticated = authService.isAuthenticated();
-  if (!isAuthenticated) {
-    router.navigate(['/login']);
-    return false;
-  }
-
-  return true;
+  return authService.isAuthenticated().pipe(
+    map((isAuthenticated) => {
+      if (!isAuthenticated) {
+        router.navigate(['/login']);
+        return false;
+      }
+      return true;
+    }),
+    catchError(() => {
+      router.navigate(['/login']);
+      return of(false); // Prevent access on error
+    })
+  );
 };
