db6c36706fae2901ae87ca121aaace3efac84f9d
Ansh Parekh <anshparekh03@gmail.com>
2024-10-11 18:09:45 +0530

    jobs table modification

--- client/src/app/components/employer-dashboard/pages/jobs/jobs.component.ts
+++ client/src/app/components/employer-dashboard/pages/jobs/jobs.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { MatTableModule,MatTableDataSource } from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableModule, MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSort, MatSortModule } from '@angular/material/sort';
 
 export interface Job {
   title: string;
@@ -17,21 +18,36 @@ const JOB_DATA: Job[] = [
   { title: 'Data Scientist', location: 'New York', role: 'Analyst', minSalary: 95000, maxSalary: 130000 },
 ];
 
+
 @Component({
+
   selector: 'app-jobs',
   standalone: true,
   imports: [
     MatPaginator,
     MatTableModule,
-    MatIconModule
+    MatIconModule,
+    MatSortModule
   ],
   templateUrl: './jobs.component.html',
   styleUrl: './jobs.component.css'
 })
 export class JobsComponent implements OnInit {
-  displayedColumns:string[]=['title','location','role','minSalary','maxSalary','actions']
-  datasource=new MatTableDataSource<Job>(JOB_DATA)
+  displayedColumns: string[] = ['title', 'location', 'role', 'minSalary', 'maxSalary', 'actions']
+  datasource = new MatTableDataSource<Job>(JOB_DATA)
+  @ViewChild(MatSort) sort!: MatSort;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
   ngOnInit() {
+
+  }
+
+  ngAfterViewInit() {
+    this.datasource.sort = this.sort;
+    this.datasource.paginator = this.paginator;
+  }
+
+  applyFilter(event:Event){
     
   }
 
