9bb45b356d15a17f9af815169495695eac8bb542
chandan kumar <chandankumar995584@gmail.com>
2024-12-04 15:28:12 +0530

    file extension checking

--- server/src/jobs/jobs.route.js
+++ server/src/jobs/jobs.route.js
@@ -12,7 +12,16 @@ const storage = multer.diskStorage({
     cb(null, file.fieldname + "-" + uniqueSuffix);
   },
 });
-const upload = multer({ storage: storage });
+
+// file filter function to only allow CSV files.
+const filterFile = (req, file, cb) => {
+  const extName = path.extname(file.originalname).toLocaleLowerCase();
+  if (extName !== ".csv") {
+    return cb(new CustomError("Only csv files are allowed", 400), false);
+  }
+  return cb(null, true);
+};
+const upload = multer({ storage: storage, fileFilter: filterFile });
 
 const {
   createJobPost,
@@ -40,6 +49,7 @@ const checkEmployerRole = require("../middleware/checkEmployerRole.middleware");
 const { validateRequest } = require("../middleware/joiValidation.middleware");
 const apiSchema = require("../utils/apiSchema");
 const checkJobseekerRole = require("../middleware/checkJobseeker.middleware");
+const { CustomError } = require("../utils/apiResponse");
 
 router
   .route("/add-jobpost")
